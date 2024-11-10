dac54c506e5bce820afd95e35cda9d67cc16a8ce
Elliott Wagener <elliott.wagener@hotmail.com>
2024-11-11 10:23:15 +1100

    Export ui components from separate file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
 export { switchEnvPlugin } from './plugin.js'
 export type { SwitchEnvPluginArgs } from './types.js'
-export {
-  SwitchEnvButtonClient,
-  type SwitchEnvButtonClientProps,
-} from './ui/components/SwitchEnvButtonClient/SwitchEnvButtonClient.js'
