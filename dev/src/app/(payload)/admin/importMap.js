a786fd689d5569caf0ed8fd8eac4f9eded8f2142
Elliott Wagener <elliott.wagener@hotmail.com>
2025-04-24 16:01:27 +1000

    Update payload versions

--- dev/src/app/(payload)/admin/importMap.js
+++ dev/src/app/(payload)/admin/importMap.js
@@ -1,7 +1,9 @@
 import { DangerBar as DangerBar_898dfb141c840c740fb4a01bfeccc518 } from '@elliott-w/payload-plugin-switch-env/client'
 import { SwitchEnvButton as SwitchEnvButton_898dfb141c840c740fb4a01bfeccc518 } from '@elliott-w/payload-plugin-switch-env/client'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 
 export const importMap = {
   "@elliott-w/payload-plugin-switch-env/client#DangerBar": DangerBar_898dfb141c840c740fb4a01bfeccc518,
-  "@elliott-w/payload-plugin-switch-env/client#SwitchEnvButton": SwitchEnvButton_898dfb141c840c740fb4a01bfeccc518
+  "@elliott-w/payload-plugin-switch-env/client#SwitchEnvButton": SwitchEnvButton_898dfb141c840c740fb4a01bfeccc518,
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24
 }
